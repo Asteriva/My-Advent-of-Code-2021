519a6b1ee656809cde058f8a2cddbc2ea3bc7d6b
Asteriva <67519722+Asteriva@users.noreply.github.com>
2021-12-17 22:42:05 +0300

    Update Main.java

--- AventOfCode/src/Day4/Main.java
+++ AventOfCode/src/Day4/Main.java
@@ -1,21 +1,7 @@
 package Day4;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
-
-
 public class Main {
 
+        // Probably gonna make this one in matlab
 
-    public static void main(String[] args) throws IOException {
-        File ff = new File("src\\Day3\\input.txt");
-        Scanner scan = new Scanner(ff);
-
-
-
-
-
-
-    }
 }
